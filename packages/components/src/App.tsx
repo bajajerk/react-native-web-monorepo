24790963fcba45a12724a62573be0422b88b5021
amitarya <amit.arya@c2fo.com>
2020-07-18 16:54:32 +0530

    [Initial Commit] Running Project

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -25,27 +25,7 @@ export function App() {
             </View>
           )}
           <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Code sharing using Monorepo</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>packages/components/App.tsx</Text> to change this
-                screen and then come back to see your edits (in the phone or the browser).
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Web support via react-native-web</Text>
-              <Text style={styles.sectionDescription}>
-                Run <Text style={styles.highlight}>yarn workspace web start</Text> to 
-                open this app in the browser. 
-              </Text>
-              <Text style={styles.sectionDescription}>
-                It will share the same code from mobile, unless you create platform-specific files 
-                using the <Text style={styles.highlight}>.web.tsx</Text> extension 
-                (also supports <Text style={styles.highlight}>.android</Text>,{' '}
-                <Text style={styles.highlight}>.ios</Text>,{' '}
-                <Text style={styles.highlight}>.native</Text>, etc).
-              </Text>
-            </View>
+            <Text >Hello</Text>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -92,4 +72,4 @@ const styles = StyleSheet.create({
   },
 });
 
-declare var global: any
+declare var global: any;
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,7 +2,7 @@ import { AppRegistry } from 'react-native'
 
 import { App } from 'components/src/App'
 
-AppRegistry.registerComponent('myprojectname', () => App)
+AppRegistry.registerComponent('myprojectname', () => App);
 AppRegistry.runApplication('myprojectname', {
   rootTag: document.getElementById('root'),
-})
+});
