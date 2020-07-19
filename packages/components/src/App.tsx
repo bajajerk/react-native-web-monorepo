5c10325b0d6bc1e5b83c3e7b90744dda20922814
amitarya <amit.arya@c2fo.com>
2020-07-19 23:06:36 +0530

    [Feature] Google signing working on Android

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -1,66 +1,22 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
-
-import { AppHeader } from './AppHeader';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from 'react-native';
+import { LoginPage } from './pages/LoginPage';
 
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: 'white',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: 'white',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: 'black',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: 'gray',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: 'gray',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
 });
 
-declare let global: any;
-
-export function App() {
+export const App: React.FC = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-          <AppHeader />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <Text>Hello</Text>
-          </View>
+          <LoginPage />
         </ScrollView>
       </SafeAreaView>
     </>
   );
-}
+};
