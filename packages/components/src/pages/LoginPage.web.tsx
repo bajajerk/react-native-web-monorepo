7b76bca8ea26a5af1b55a2d5b12febf207bb8eeb
mayank <bajaj3743@gmail.com>
2020-07-24 18:52:19 +0530

    [FEATURE] Google login web

--- packages/components/src/pages/LoginPage.web.tsx
+++ packages/components/src/pages/LoginPage.web.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 
+
+
+const responseGoogle = (response) => {
+    console.log(response);
+}
 
 export const LoginPage: React.FC = () => {
-    return <p> Rendered in web, @Amit Arya write here </p>;
+    return (
+        <GoogleLogin
+            clientId="694785323635-p5kot1gclqg89kljalkdcc3735fvkev4.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+        />
+    )
 };
